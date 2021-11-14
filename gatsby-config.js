c5a6c2b09eca3244a56040fc2ff34d5e2f6d63dd
Rowee <lee9uanlin@gmail.com>
2021-11-14 01:17:32 -0800

    Add scripts to deploy to gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     {
       resolve: "gatsby-theme-portfolio-minimal",
       options: {
-        siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
+        siteUrl: "https://9uanlin.github.io", // Used for sitemap generation
         manifestSettings: {
           favicon: "./content/images/favicon.png", // Path is relative to the root
           siteName: "9uanlin Portfolio", // Used in manifest.json
